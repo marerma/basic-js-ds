8e3b2dc6e4559350111945fad0036c758aa098e5
Maria Ermolaeva <maria.kadantseva@gmail.com>
2022-10-12 17:29:02 +0300

    feat: add solution stack and queue

--- src/stack.js
+++ src/stack.js
@@ -13,21 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.arr = []
+  }
+
+  push(n) {
+    this.arr.push(n)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
+    return this.arr[this.arr.length - 1]
+ }
+ }
+
+
 
 module.exports = {
   Stack
